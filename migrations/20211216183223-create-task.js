e2cb5aed51675d1092cebc48e63ed89716c899db
Martin Cappi <mcappi91@gmail.com>
2021-12-16 15:35:09 -0300

    sequelize-cli: create Task model
    sequelize-cli model:generate --name Task  --attributes summary:string,description:string,completed:boolean,fk_user:integer

---
+++ migrations/20211216183223-create-task.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      summary: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      fk_user: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
