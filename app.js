6c5cb0f1074f8b46cbc4d56e83d676352e344235
Martin Cappi <mcappi91@gmail.com>
2021-12-16 15:13:00 -0300

    set cors + set up error middleware

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
+
+const { notFound, errorHandler } = require("./middleware/ErrorMiddleware.js");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
