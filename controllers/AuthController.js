2854cc103abef70de2c9f3f9af296da6774aeda1
Martin Cappi <mcappi91@gmail.com>
2021-12-16 19:24:11 -0300

    AuthController: Login + Register

---
+++ controllers/AuthController.js
@@ -0,0 +1,56 @@
+const { generateToken } = require('../helpers/auth/generateToken');
+const { CompareHash } = require('../helpers/auth/hash');
+const db = require('../models/index');
+
+const { User } = db.sequelize.models;
+
+const Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    const isMatch = user && (await CompareHash(password, user.password));
+
+    if (isMatch) {
+      const token = generateToken(user);
+      res.status(200).json(token);
+    } else {
+      res.status(401);
+      throw new Error('Invalid credentials!');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const Register = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      res.status(409);
+      throw new Error('This email is already registered!');
+    }
+
+    const newUser = await User.create(
+      {
+        email,
+        password
+      },
+      {
+        validation: true,
+        fields: ['email', 'password']
+      }
+    );
+
+    const token = generateToken(newUser);
+
+    res.status(201).json(token);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { Login, Register };
